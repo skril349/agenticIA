d412668b0503f68161f5bdedad83e9b73cdbc03e
skril349 <tvives.cabaleiro@gmail.com>
2026-09-16 09:58:01 +0200

    pointing to aws

--- week2/day1/twin/frontend/components/twin.tsx
+++ week2/day1/twin/frontend/components/twin.tsx
@@ -40,7 +40,7 @@ export default function Twin() {
         setIsLoading(true);
 
         try {
-            const response = await fetch('http://localhost:8000/chat', {
+            const response = await fetch('https://vlv8gp0q58.execute-api.eu-west-1.amazonaws.com/chat', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
